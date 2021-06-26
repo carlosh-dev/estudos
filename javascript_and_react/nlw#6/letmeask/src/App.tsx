4fedb0144bee8e291d626a9683c21ae812789b5b
unknown <caique.h@hotmail.com>
2021-06-25 22:19:04 -0300

    NLW aula #3

--- javascript_and_react/nlw#6/letmeask/src/App.tsx
+++ javascript_and_react/nlw#6/letmeask/src/App.tsx
@@ -1,6 +1,21 @@
+// IMPORTANT: install @types/react-router-dom for Typescript purposes
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from "./contexts/AuthContexts";
+import { Room } from './pages/Room';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/room/new" component={NewRoom}/>
+          <Route path="/room/:id" component={Room}/>
+        </Switch>
+      </AuthContextProvider>   
+    </BrowserRouter>
   );
 }
 
--- javascript_and_react/nlw#6/letmeask/src/index.tsx
+++ javascript_and_react/nlw#6/letmeask/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import "./services/firebase.tsx"
+import "./styles/global.scss"
 
 ReactDOM.render(
   <React.StrictMode>
